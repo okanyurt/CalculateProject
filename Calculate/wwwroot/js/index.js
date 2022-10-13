4d1115a1a880a068c02e1b9f6e2f1977046fc3e2
okanyurt <okan.yurt@hotmail.com>
2022-10-13 14:29:20 +0300

    web uygulaması theme giydirilmesi yapıldı.

---
+++ Calculate/wwwroot/js/index.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    $.noConflict();
+    $('#example').DataTable({
+        dom: 'Bfrtip',
+        buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+        ]
+    });
+});
