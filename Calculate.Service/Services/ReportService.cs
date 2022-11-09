d4261f335b9694e7dcdef0a7d8b7a1e54e92fb4f
okanyurt <okan.yurt@hotmail.com>
2022-11-09 22:59:42 +0300

    case report table reorganized

--- Calculate.Service/Services/ReportService.cs
+++ Calculate.Service/Services/ReportService.cs
@@ -1,6 +1,8 @@
 ﻿using Calculate.Data;
+using Calculate.Data.Enums;
 using Calculate.Data.Models;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
 
 namespace Calculate.Service.Services
 {
@@ -71,14 +73,14 @@ namespace Calculate.Service.Services
 
         public async Task<List<ReportGet>> GetAllForCaseAsync(int Id)
         {
+            var today = DateTime.UtcNow.Date;
             var list = from o in _context.Operations
                        join a in _context.Accounts on o.AccountId equals a.Id
                        join ad in _context.AccountDetails on o.AccountDetailId equals ad.Id
                        join b in _context.Banks on ad.BankId equals b.Id
                        join pt in _context.ProcessTypes on o.ProcessTypeId equals pt.Id
                        join c in _context.Cases on o.CaseId equals c.Id
-                       where o.IsEnable == true && o.CaseId == Id
-                       orderby o.UpdatedDate descending
+                       where o.IsEnable == true && o.CaseId == Id && o.UpdatedDate.Date == today                    
                        group new { a, b, pt, o } by new
                        {
                            Account = a.Name,
@@ -91,12 +93,36 @@ namespace Calculate.Service.Services
                            AccountDetail = g.Key.AccountDetail,
                            ProcessType = g.Key.ProcessType,
                            Price = g.Sum(x => x.o.Price),
+                           ProcessPrice = g.Sum(x => x.o.ProcessPrice),
                            ProcessCount = g.Count()
                        };
 
+            var raporList = await list.ToListAsync();
 
-            return await list.ToListAsync();
-        }
-        
+            List<string> minusAccount = new List<string>() { "ÇEKİM", "KOMİSYON", "TRANSFER" };
+
+            List<string> minusProcessCount = new List<string>() { "YATIRIM" };
+
+            raporList = raporList.Select(item => new ReportGet
+            {
+                Account = item.Account,
+                AccountDetail = item.AccountDetail,
+                ProcessType = item.ProcessType,
+                Price = minusAccount.Contains(item.ProcessType) ? -1 * item.Price : item.Price,
+                ProcessPrice = -1 * item.ProcessPrice,
+                ProcessCount = minusProcessCount.Contains(item.ProcessType) ? item.ProcessCount : 0 * item.ProcessCount
+            }).ToList();
+
+            raporList = raporList.GroupBy(x => new { x.Account, x.AccountDetail }).Select(x => new ReportGet
+            {
+                Account = x.Key.Account,
+                AccountDetail = x.Key.AccountDetail,               
+                Price = x.Sum(y => y.Price) + x.Sum(y => y.ProcessPrice),
+                ProcessPrice = 0,
+                ProcessCount = x.Sum(y => y.ProcessCount)
+            }).OrderBy(x => x.Account).ToList();
+
+            return raporList;
+        }    
     }
 }
